06849f682c372931a006c49eee8d2ebba48fe7d7
Cynthia Salazar <cinthya3259@gmail.com>
2021-02-06 19:25:29 -0600

    Versión count

--- exámen.rb
+++ exámen.rb
@@ -15,14 +15,27 @@ def web_page_nasa(request)
     images = request.map do |filtering|
         filtering['img_src']
     end
-    html = "<html>\n<head>\n</head>\n\t<body>\n\t\t<ul>"
+    html = "<html>\n<h1> Mars  </h1>\n<head>\n</head>\n\t<body>\n\t\t<ul>"
     images.each do |image|
-        html += "\n\t\t\t<li><img src=#{image}'></li>\n"
+        html += "\n\t\t\t<li><img src='#{image}'width ='700'></li>\n"
     end
     html += "\t\t</ul>\n\t</body>\n</html>"
     return html
 end
 
+photos = web_page_nasa(request('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=10')["photos"])
+File.write('mars_photos.html', photos.to_s)
 
-nasa_array = request("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=10")
-puts request(nasa_array)
+def photos_count(request)
+    camera_name = request.map do |filtering|
+        filtering['camera']['name']
+    end
+    group = camera_name.group_by { |camera_name| camera_name }
+    number_photo = group.each do |cam_group, photo_qty|
+        group[cam_group] = photo_qty.count
+    end
+    return number_photo
+end
+
+print photos_count(request('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=10')["photos"])
+
