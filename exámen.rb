82005a8b65a12cd0e6d6fe7ae65b0fe097074333
Cynthia Salazar <cinthya3259@gmail.com>
2021-02-06 18:21:56 -0600

    Cuerpo

--- exámen.rb
+++ exámen.rb
@@ -2,8 +2,7 @@ require "uri"
 require "net/http"
 require "json"
 
-
-def request(url, apy_key="Z5CuehDG6UfLpxQnPyyIDCO2ChxNZF55iGxkgP2d")
+def request(url, api_key="Z5CuehDG6UfLpxQnPyyIDCO2ChxNZF55iGxkgP2d")
     url = URI("#{url}&api_key=#{api_key}")
     https = Net::HTTP.new(url.host, url.port)
     https.use_ssl = true
@@ -11,7 +10,19 @@ def request(url, apy_key="Z5CuehDG6UfLpxQnPyyIDCO2ChxNZF55iGxkgP2d")
     response = https.request(request)
     JSON.parse (response.read_body)
 end
-    
 
-response = http.request(request("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=10")
-puts response.read_body 
+def web_page_nasa(request)
+    images = request.map do |filtering|
+        filtering['img_src']
+    end
+    html = "<html>\n<head>\n</head>\n\t<body>\n\t\t<ul>"
+    images.each do |image|
+        html += "\n\t\t\t<li><img src=#{image}'></li>\n"
+    end
+    html += "\t\t</ul>\n\t</body>\n</html>"
+    return html
+end
+
+
+nasa_array = request("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=10")
+puts request(nasa_array)
