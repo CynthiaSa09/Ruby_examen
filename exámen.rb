a368ef5de2b159aee3302b0436fd95408f756066
Cynthia Salazar <cinthya3259@gmail.com>
2021-02-08 08:37:51 -0600

    Versión final

--- exámen.rb
+++ exámen.rb
@@ -4,23 +4,25 @@ require "json"
 
 def request(url, api_key="Z5CuehDG6UfLpxQnPyyIDCO2ChxNZF55iGxkgP2d")
     url = URI("#{url}&api_key=#{api_key}")
-    https = Net::HTTP.new(url.host, url.port)
-    https.use_ssl = true
+    http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = true
     request = Net::HTTP::Get.new(url)
-    response = https.request(request)
+    response = http.request(request)
     JSON.parse (response.read_body)
+#. Crear el método request que reciba una url y el api_key y devuelva el hash con los resultados.
 end
 
 def web_page_nasa(request)
     images = request.map do |filtering|
         filtering['img_src']
     end
-    html = "<html>\n<h1> Mars  </h1>\n<head>\n</head>\n\t<body>\n\t\t<ul>"
+    html = "<html>\n<h1> Mars Photos </h1>\n<head>\n</head>\n\t<body>\n\t\t<ul>"
     images.each do |image|
         html += "\n\t\t\t<li><img src='#{image}'width ='700'></li>\n"
     end
     html += "\t\t</ul>\n\t</body>\n</html>"
     return html
+#Crear un método que reciba el hash de respuesta con todos los datos y construya una página web.
 end
 
 photos = web_page_nasa(request('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=10')["photos"])
@@ -35,6 +37,7 @@ def photos_count(request)
         group[cam_group] = photo_qty.count
     end
     return number_photo
+#Crear un método que reciba el hash de respuesta y devuelva un nuevo hash con el nombre de la camara y la cantidad de fotos
 end
 
 print photos_count(request('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=10')["photos"])
